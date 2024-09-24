dc4ee591bc6b92f9e85804cd8dc9fa7728bf6001
AKOREDE RIDWANULLAHI ABIODUN <78937801+akoredera@users.noreply.github.com>
2024-09-24 11:29:03 +0100

    modified

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,18 +3,6 @@
 island perimeter
 '''
 
-# def island_perimeter(grid):
-#     '''
-#     island perimeter
-#     '''
-#     new_list = []
-#     for i in range(len(grid)):
-#         for row in grid:
-#             if i > 0 and i < len(grid):
-#                 if row != grid[0] and row != grid[-1]:
-
-#                     new_list.append(row[i])
-#     return len(new_list)
 def check_val(x):
     """_summary_
 
